3feae85f4095ddb62be69c72e817cff65b470b52
lihaonan04 <lihaonan04@58ganji.com>
2019-01-05 17:34:44 +0800

    增加axios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App'
 import router from './router'
 // vant组件
@@ -8,6 +10,8 @@ import './vant'
 // 全局组件
 import './components/global'
 import 'style/global.scss'
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
